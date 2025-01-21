b59373051d369602f6cc33974a54cca5cf8870f4
Ray <lujiyun@sensing-world.com>
2025-01-21 15:25:33 +0800

    Updated SG4A-NONX-G2Y-A1 adapter board device tree deployment script.

--- Jetson Orin Nano&NX Devkit/SG4A-NONX-G2Y-A1/JetPack6.1/SG4A-NONX-G2Y-A1_ORIN_NANO_YUV_JP6.1_L4TR36.4.2/install.sh
+++ Jetson Orin Nano&NX Devkit/SG4A-NONX-G2Y-A1/JetPack6.1/SG4A-NONX-G2Y-A1_ORIN_NANO_YUV_JP6.1_L4TR36.4.2/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo cp boot/Image /boot/Image
+sudo cp dtb/SGX_YUV_GMSL2/tegra234-camera-yuv-gmsl2x4-nano-overlay.dtbo /boot/tegra234-camera-yuv-gmsl2x4-nano-overlay.dtbo
+
 sudo cp ./ko/tegra-camera.ko /lib/modules/5.15.148-tegra/updates/drivers/media/platform/tegra/camera/
 sudo cp ./ko/nvhost-nvcsi-t194.ko /lib/modules/5.15.148-tegra/updates/drivers/video/tegra/host/nvcsi/
 
