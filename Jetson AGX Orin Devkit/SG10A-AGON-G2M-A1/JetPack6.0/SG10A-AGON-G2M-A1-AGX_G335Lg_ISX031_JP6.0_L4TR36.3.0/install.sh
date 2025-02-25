b33166ed869bb380576344fb8bcd41ba28efea12
Ray <lujiyun@sensing-world.com>
2025-02-25 17:30:34 +0800

    Added AGX Orin support for SG10A-AGON-G2M-A1 adapter board JetPack 6.0 version driver.

---
+++ Jetson AGX Orin Devkit/SG10A-AGON-G2M-A1/JetPack6.0/SG10A-AGON-G2M-A1-AGX_G335Lg_ISX031_JP6.0_L4TR36.3.0/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo cp boot/Image /boot/Image
+
+sudo cp dtb/SGX_G2XX_ISX031/tegra234-p3737-0000+p3701-0000-nv.dtb /boot/dtb/kernel_tegra234-p3737-0000+p3701-0000-nv.dtb
+sudo cp dtb/SGX_G2XX_ISX031/tegra234-camera-g2xx-isx031-overlay.dtbo /boot/tegra234-camera-g2xx-isx031-overlay.dtbo
+
+sudo cp ./ko/tegra-camera.ko /lib/modules/5.15.136-tegra/updates/drivers/media/platform/tegra/camera/
+sudo cp ./ko/nvhost-nvcsi-t194.ko /lib/modules/5.15.136-tegra/updates/drivers/video/tegra/host/nvcsi/
+
+sudo cp ./ko/capture-ivc.ko /lib/modules/5.15.136-tegra/updates/drivers/platform/tegra/rtcpu/
+
+sudo cp ./ko/videodev.ko /lib/modules/5.15.136-tegra/kernel/drivers/media/v4l2-core/
+sudo cp ./ko/uvcvideo.ko /lib/modules/5.15.136-tegra/kernel/drivers/media/usb/uvc/
+
+sudo rm -rf /lib/modules/5.15.136-tegra/updates/drivers/media/i2c/max9296.ko
+sudo rm -rf /lib/modules/5.15.136-tegra/updates/drivers/media/i2c/max9295.ko
+sudo rm -rf /lib/modules/5.15.136-tegra/updates/drivers/media/i2c/max96712.ko
+
+sync
+
+
