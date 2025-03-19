6cdf5d453a3217ffa47481cd4a22208759e288af
Ray <lujiyun@sensing-world.com>
2025-03-19 16:39:24 +0800

    Upload the JetPack 6.2 driver adapted for two SG3-ISX031C-MIPI cameras.

---
+++ Jetson Orin Nano&NX Devkit/SG_MIPI_CAM/JetPack6.2/Jetson-Orin-Nano-DK_ISX031x2_2LANE_YUV_JP6.2_L4TR36.4.3/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo cp boot/Image /boot/Image
+sudo cp dtb/SG3_MIPI_ISX031/tegra234-camera-isx031-mipi-2lane-overlay.dtbo /boot/tegra234-camera-isx031-mipi-2lane-overlay.dtbo
+sudo cp ./ko/tegra-camera.ko /lib/modules/5.15.148-tegra/updates/drivers/media/platform/tegra/camera/
+sudo cp ./ko/nvhost-nvcsi-t194.ko /lib/modules/5.15.148-tegra/updates/drivers/video/tegra/host/nvcsi/
+
+sudo rm -rf /lib/modules/5.15.148-tegra/updates/drivers/media/i2c/max9296.ko
+sudo rm -rf /lib/modules/5.15.148-tegra/updates/drivers/media/i2c/max9295.ko
+sudo rm -rf /lib/modules/5.15.148-tegra/updates/drivers/media/i2c/max96712.ko
