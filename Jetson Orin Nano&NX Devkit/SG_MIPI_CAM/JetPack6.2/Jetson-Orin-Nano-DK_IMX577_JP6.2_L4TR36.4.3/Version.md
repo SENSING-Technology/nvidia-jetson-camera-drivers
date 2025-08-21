4e07015db53876f87fba1c74e0083bc4642951de
Ray <lujiyun@sensing-world.com>
2025-08-21 09:48:36 +0800

    Upload the Jetson Orin Nano compatible IMX577 MIPI camera driver package (version 6.2)

---
+++ Jetson Orin Nano&NX Devkit/SG_MIPI_CAM/JetPack6.2/Jetson-Orin-Nano-DK_IMX577_JP6.2_L4TR36.4.3/Version.md
@@ -0,0 +1,10 @@
+#### Version
+
+* 2025.08.20
+
+#### Supported SENSING Camera Modules
+
+* SG12-IMX577C-MIPI-HXXX
+
+  * support max 2 cameras to light up at the same time
+
---
+++ Jetson Orin Nano&NX Devkit/SG_MIPI_CAM/JetPack6.2/Jetson-Orin-Nano-DK_IMX577_JP6.2_L4TR36.4.3/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo cp boot/Image /boot/Image
+sudo cp dtb/SG12_MIPI_IMX577/tegra234-camera-imx577-mipi-overlay.dtbo /boot/tegra234-camera-imx577-mipi-overlay.dtbo
+
+sudo cp ./ko/tegra-camera.ko /lib/modules/5.15.148-tegra/updates/drivers/media/platform/tegra/camera/
+sudo cp ./ko/nvhost-nvcsi-t194.ko /lib/modules/5.15.148-tegra/updates/drivers/video/tegra/host/nvcsi/
+
+sudo rm -rf /lib/modules/5.15.148-tegra/updates/drivers/media/i2c/max9296.ko
+sudo rm -rf /lib/modules/5.15.148-tegra/updates/drivers/media/i2c/max9295.ko
+sudo rm -rf /lib/modules/5.15.148-tegra/updates/drivers/media/i2c/max96712.ko
