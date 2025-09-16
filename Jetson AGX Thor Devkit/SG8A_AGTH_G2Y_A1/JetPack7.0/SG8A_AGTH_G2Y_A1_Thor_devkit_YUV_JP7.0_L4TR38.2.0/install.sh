3dc3e05b3d2953b1046fb0d84c590190ec40af62
wangyong <wangyong@sensing-world.com>
2025-09-16 09:44:56 +0800

    Upload Jetson AGX Thor Devkit JetPack7.0 driver package for the SG8A_AGTH_G2Y_A1

---
+++ Jetson AGX Thor Devkit/SG8A_AGTH_G2Y_A1/JetPack7.0/SG8A_AGTH_G2Y_A1_Thor_devkit_YUV_JP7.0_L4TR38.2.0/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update ko
+sudo cp ko/tegra-camera.ko /lib/modules/6.8.12-tegra/updates/drivers/media/platform/tegra/camera/
+sudo cp ko/nvhost-nvcsi.ko /lib/modules/6.8.12-tegra/updates/drivers/video/tegra/host/nvcsi/
+sudo rm -rf /lib/modules/6.8.12-tegra/updates/drivers/media/i2c/max96712.ko
+
+# add dtbo
+sudo cp dtb/SGX_YUV_GMSL2/tegra264-camera-yuv-gmsl2x4-overlay.dtbo /boot/
+
+# upgrade Image
+sudo cp boot/Image /boot/Image
+
+sync
